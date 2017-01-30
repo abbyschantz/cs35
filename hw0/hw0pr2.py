f4086ac890ee55c5ecf5c14f970685650dbafb9a
ekeinan <ekeinan17@cmc.edu>
2017-01-29 21:54:03 -0800

    added count_garcia_by_areacode and count42

--- hw0/hw0pr2.py
+++ hw0/hw0pr2.py
@@ -136,7 +136,7 @@ def count_garcia():
 
 
 def isGarcia(filename):
-    """ checks if the number is in the area code 909
+    """ checks if the file contains the name Garcia
     """
 
     file = open(filename,"r")
@@ -147,12 +147,85 @@ def isGarcia(filename):
         return True
 
 
+def contains42(filename):
+    """ checks if the phone number has the substring 42 in it 
+    """
+
+    file = open(filename, "r")
+    text = file.read()
+    text = clean_digits(text)
+
+    if '42' in text:
+        return True
+
+
+def count_42():
+    """ returns the number of phone numbers that contain 42
+    """
+
+    L = os.listdir("phone_files") 
+    os.chdir( "phone_files" )
+    
+    count = 0
+
+    for foldername in L[1:]:
+        files = os.listdir(foldername)  
+        os.chdir(foldername)
+
+        for filename in files:
+            if contains42(filename):
+                count += 1
+
+        os.chdir("..")
+
+    os.chdir("..") 
+    return count
+
+
+def count_garcia_by_areacode():
+    """ returns the number of "GARCIA"s in each area code
+    """
+
+    L = os.listdir("phone_files") 
+    os.chdir( "phone_files" )
+    
+    d = {}
+    count = 0
+
+    for foldername in L[1:]:
+        files = os.listdir(foldername)  
+        os.chdir(foldername)
+
+        for filename in files:
+            if isGarcia(filename):
+                if areacode(filename) in d:
+                    d[areacode(filename)] += 1
+                else:
+                    d[areacode(filename)] = 1
+
+        os.chdir("..")
+
+    os.chdir("..") 
+    return d
+
+
+def areacode(filename):
+    """ returns the areacode
+    """
+    file = open(filename,"r")
+    text = file.read()
+    
+    text = clean_digits(text)
+    return text[0:3]
+
+
 
 def main():
     print("There are ", count_files(), " .txt files in the whole set.")
     print("There are ", count_ten(), " phone numbers that are exactly 10 digits.")
     print("There are ", count_909(), " phone numbers that are in 909.")
     print("There are ", count_garcia(), " people with the name 'GARCIA' in the whole set.")
+    print("There are ", count_42(), "phone numbers that contain the string '42'.")
 
 """
 ANSWERS:
@@ -161,6 +234,7 @@ ANSWERS:
 2. There are 3988 phone numbers that are exactly 10 digits.
 3. There are 9 phone numbers that are in 909.
 4. There are 237 people with the name "GARCIA" in the whole set.
+5. There are 671 phone numbers that contain the string "42".
 
 
 """
