ac5d8a48ed474a2ac2ffe7b0275cfdc7090adcfa
ekeinan <ekeinan17@cmc.edu>
2017-02-05 20:49:45 -0800

    updates to hw1pr2

--- hw1/cs35_hw1_all_starters/hw1pr2_starter.py
+++ hw1/cs35_hw1_all_starters/hw1pr2_starter.py
@@ -1,5 +1,5 @@
 #
-# starter file for hw1pr2, cs35 spring 2017...
+# Names: Eliana Keinan, Abby Schantz, Liz Harder
 # 
 
 import csv
@@ -80,3 +80,130 @@ def csv_to_html_table_starter( csvfilename ):
     return html_string
 
 
+def first_letter_counter_weighted(csv_file_name):
+    """ returns the weighted count of words that start with each letter
+    """
+
+    L = readcsv(csv_file_name)
+    d = {}
+
+    for word in L:
+        word[0] = word[0].lower()
+        if ord(word[0][0]) >= ord('a') and ord(word[0][0]) <= ord('z'):
+            if word[0][0] in d:
+                d[word[0][0]] += 1*(float(word[1]))
+            else:
+                d[word[0][0]] = 1*(float(word[1]))
+
+    return d
+
+
+def last_letter_counter_weighted(csv_file_name):
+    """ returns the weighted count of words that end with each letter
+    """
+
+    L = readcsv(csv_file_name)
+    d = {}
+
+    for word in L:
+        word[0] = word[0].lower()
+        if ord(word[0][-1]) >= ord('a') and ord(word[0][-1]) <= ord('z'):
+            if word[0][-1] in d:
+                d[word[0][-1]] += 1*(float(word[1]))
+            else:
+                d[word[0][-1]] = 1*(float(word[1]))
+
+    return d
+
+
+def first_last_equal_counter_weighted(csv_file_name):
+    """ returns the weighted count of words (longer than 1 letter) that start and end with the same letter
+    """
+
+    L = readcsv(csv_file_name)
+    d = {}
+
+    for word in L:
+        word[0] = word[0].lower()
+        if ord(word[0][0]) >= ord('a') and ord(word[0][0]) <= ord('z'):
+            if word[0][0] == word[0][-1] and len(word[0]) > 1:
+                if word[0][0] in d:
+                    d[word[0][0]] += 1*(float(word[1]))
+                else:
+                    d[word[0][0]] = 1*(float(word[1]))
+
+    return d
+
+
+def output_first_letter(csv_file_name):
+    """ outputs a csv file for the first letter counter
+    """
+    D = first_letter_counter_weighted(csv_file_name)
+    
+    list_of_rows = []
+    for key, value in D.items():
+        list_of_rows.append((key,value))
+    
+    list_of_rows.sort()
+    
+    write_to_csv( list_of_rows, "firstletter.csv")
+
+def output_last_letter(csv_file_name):
+    """ outputs a csv file for the last letter counter
+    """
+    D = last_letter_counter_weighted(csv_file_name)
+    
+    list_of_rows = []
+    for key, value in D.items():
+        list_of_rows.append((key,value))
+    
+    list_of_rows.sort()
+    
+    return write_to_csv( list_of_rows, "lastletter.csv")
+
+
+def output_first_last_letter(csv_file_name):
+    """ outputs a csv file for the first last equal counter
+    """
+    D = first_last_equal_counter_weighted(csv_file_name)
+    
+    list_of_rows = []
+    for key, value in D.items():
+        list_of_rows.append((key,value))
+    
+    list_of_rows.sort()
+    
+    return write_to_csv( list_of_rows, "firstlastletter.csv")
+
+
+### FIX THIS STILL ###
+def csv_to_html_table( csvfilename ):
+    """ 
+    """
+   
+   result = csv_to_html_table_starter("FILE")
+   print(result)
+   
+   
+    # probably should use the readcsv function, above!
+    html_string = '<table>\n'    # start with the table tag
+    html_string += '<tr>\n'
+
+    html_string += "place structured data here!\n" # from list_of_rows !
+
+    html_string += '</tr>\n'
+    html_string += '</table>\n'
+    return html_string
+
+
+
+
+def main():
+    """ creates three files that provide different analysis on letter frequencies
+    """
+    
+    csv_file_name = "wds.csv"
+    
+    output_first_letter(csv_file_name)
+    output_last_letter(csv_file_name)
+    output_first_last_letter(csv_file_name)
