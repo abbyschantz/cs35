9314824d943ba4505a1599737632c783ac094647
abbyschantz <abby.schantz@gmail.com>
2017-03-05 19:36:33 -0800

    working 2 sorta'

--- hw5/titanic5.py
+++ hw5/titanic5.py
@@ -1,37 +1,37 @@
-#titanic5.py
-
 #
 #
-
-#from sklearn.datasets import load_titanic
-#titanic = load_titanic()
-
+# titanic5.py
+#
+#
 
 import numpy as np
+from sklearn import datasets
 from sklearn import cross_validation
+import pandas as pd
 from sklearn import tree
 from sklearn import ensemble
-import pandas as pd
 
-print("+++ Start of pandas' datahandling +++\n")
+# For Pandas's read_csv, use header=0 when you know row 0 is a header row
 # df here is a "dataframe":
-df = pd.read_csv('titanic5.csv', header=0)    # read the file w/header row #0
-df.head()                                 # first five lines
-df.info()                                 # column details
-
-df = df.drop('name', axis=1)  # axis = 1 means column
-df = df.drop('ticket', axis=1)
-df = df.drop('cabin', axis=1)
-df = df.drop('home.dest', axis=1)
-
-df = df.dropna()
+df = pd.read_csv('titanic5.csv', header=0)
 df.head()
 df.info()
 
+# let's drop columns with too few values or that won't be meaningful
+df = df.drop('name', axis=1)
+df = df.drop('ticket', axis=1)
+df = df.drop('fare', axis=1)
+df = df.drop('cabin', axis=1)
+df = df.drop('embarked', axis=1)
+df = df.drop('home.dest', axis=1)
 
-# One important feature is the conversion from string to numeric datatypes!
-# As input features, numpy and scikit-learn need numeric datatypes
-# You can define a transformation function, to help out...
+
+
+# let's drop all of the rows with missing data:
+df = df.dropna()
+
+
+# converts string data of sex to numerical data, 0 and 1
 def tr_mf(s):
     """ from string to number
     """
@@ -40,81 +40,52 @@ def tr_mf(s):
 
 df['sex'] = df['sex'].map(tr_mf)  # apply the function to the column
 
-
-def tr_embark(s):
-	d = {'S': 0, 'C': 1, 'Q': 2}
-	return d[s]
-df['embarked'] = df['embarked'].map(tr_embark)
-# let's see our dataframe again...
-df.head()
-df.info()
-
-print("\n+++ End of pandas +++\n")
-
-print("+++ Start of numpy/scikit-learn +++\n")
-
-
+# We'll stick with numpy - here's the conversion to a numpy array
 
 # extract the underlying data with the values attribute:
 X_data = df.drop('survived', axis=1).values        # everything except the 'survival' column
 y_data = df[ 'survived' ].values      # also addressable by column name(s)
 
-
-# Data needs to be in numpy arrays - these next two lines convert to numpy arrays
-X_data_orig = df.iloc[:,0:7].values        # iloc == "integer locations" of rows/cols
-y_data_orig = df[ 'age' ].values      # individually addressable columns (by name)
-
-###############
-feature_names = df.columns.values          # get the names into a list!
-target_names = ['0','1']   # and a list of the labels...
-
-#make list of ages for target_numbers
-#age_list = []
-#for number in range(0,100):
-	#age_list.append(str(number))
-
-#add decinmal ages to the list of ages
-#for age in y_data:
-#	if age < 1:
-#		age_list.append(str(age))
-
-
-feature_names = df.columns.values          # get the names into a list!
-target_names = age_list   # and a list of the labels... (from age list)
-print (target_names)
-
-################################################
-
-X_data_full = X_data_orig[0:,:]  # make the 10 into 0 to keep all of the data
-y_data_full = y_data_orig[0:]    # same for this line
 #
-# we can scramble the data - but only if we know the test set's labels!
-# 
-indices = np.random.permutation(len(X_data_full))  # this scrambles the data each time
-X_data_full = X_data_full[indices]
-y_data_full = y_data_full[indices]
-
+# you can take away the top 42 passengers (with unknown survival/perish data) here:
 #
-# The first ten will be our test set - the rest will be our training set
-#
-X_test = X_data_full[0:30,0:7]              # the final testing data
-X_train = X_data_full[30:,0:7]              # the training data
+X_data_full = X_data[42:,:]
+y_data_full = y_data[42:]
 
-y_test = y_data_full[0:30]                  # the final testing outputs/labels (unknown)
-y_train = y_data_full[30:]                  # the training outputs/labels (known)
+indicies = np.random.permutation(len(X_data_full))
+X_data_full = X_data_full[indicies]
+y_data_full = y_data_full[indicies]
 
+
+X_test = X_data_full[0:42,0:12]             # the final testing data
+X_train = X_data_full[42:,0:12]             # the training data
+y_test = y_data_full[0:42]                  # the final testing outputs/labels (unknown)
+y_train = y_data_full[42:]                  # the training outputs/labels (known)
+
+# feature engineering...
+X_data[:,0] *= 550   # pclass
+X_data[:,1] *= 450   # sex
+X_data[:,2] *= 25  	 # age
+X_data[:,3] *= 20    # sibs
+X_data[:,4] *= 1     # parch
+
+print('\n')
+print("DT ANALYSIS\n")
 #
 # cross-validation to determine the Decision Tree's parameter (to find max_depth)
 #
-max_depth=4
+max_depth=10
 #
 
+depth = 0
 best_score = 0
-index = 0
-for i in range(1,11):
-    score = 0
-    dtree = tree.DecisionTreeRegressor(max_depth=i)
-    for j in range(1,11):  # run at least 10 times.... take the average cv testing score
+
+for n in range(1,max_depth+1):
+
+    dtree = tree.DecisionTreeClassifier(max_depth=n)
+    avg_score = 0
+        
+    for i in range(10):  # runs 10 times
         #
         # split into our cross-validation sets...
         #
@@ -125,105 +96,71 @@ for i in range(1,11):
         #   typically cross-validation is used to get a sense of how well it works
         #   and tune any parameters, such as the k in kNN (3? 5? 7? 41?, etc.)
         dtree = dtree.fit(cv_data_train, cv_target_train) 
-        #print("CV training-data score:", dtree.score(cv_data_train,cv_target_train))
-        #print("CV testing-data score:", dtree.score(cv_data_test,cv_target_test))
-        score += dtree.score(cv_data_test,cv_target_test)
-        #print("score is ", score)
-    average_score = score/10
-    print(i,average_score)
-    if average_score > best_score:
-        best_score = average_score
-        index = i
-print("DT best score is ", best_score)
+        avg_score += dtree.score(cv_data_test,cv_target_test)
+    
+    avg_score = avg_score / 10
 
-#import sys
-#sys.exit(0)
+    print(n, ":", avg_score)
+    #print("Average DT testing score at depth ++",n,"++ was: ", avg_score)
 
-# dtree.feature_importances_  [already computed]
+    if best_score < avg_score:
+        best_score = avg_score
+        depth = n
+
+print("The best DT score was ", best_score, "with a depth of ", depth)
+
 
 
 #
 # now, train the model with ALL of the training data...  and predict the labels of the test set
 #
 
-X_test = X_data_orig[0:30,0:7]              # the final testing data
-X_train = X_data_orig[30:,0:7]              # the training data
 
-y_test = y_data_orig[0:30]                  # the final testing outputs/labels (unknown)
-y_train = y_data_orig[30:]                  # the training outputs/labels (known)
-
-#
-# show the creation of three tree files (three max depths)
-#
-best = 0
-for max_depth in [1,2,3,4]:
-    #
-    # we'll use max_depth between 1 and 3
-    #
-    dtree = tree.DecisionTreeRegressor(max_depth=max_depth)
-
-
-    # this next line is where the full training data is used for the model
-    dtree = dtree.fit(X_data_full, y_data_full) 
-    print("\nCreated and trained a knn Regressor")  #, knn
-
-
-    #
-    # write out the dtree to tree.dot (or another filename of your choosing...)
-    tree.export_graphviz(dtree, out_file='tree' + str(max_depth) +'.dot',   # constructed filename!
-                            feature_names=feature_names,  filled=True, rotate=False, # LR vs UD
-                            class_names=target_names, leaves_parallel=True)  
-    # the website to visualize the resulting graph (the tree) is at www.webgraphviz.com
-    #
+#rerun with optimal depth
+dtree = tree.DecisionTreeClassifier(max_depth=depth)
+cv_data_train, cv_data_test, cv_target_train, cv_target_test = \
+            cross_validation.train_test_split(X_train, y_train, test_size=0.2) # random_state=0 
+dtree = dtree.fit(cv_data_train, cv_target_train) 
+score = dtree.score(cv_data_test,cv_target_test)
 
 
 
-# here are some examples, printed out:
-print("titanic_X_test's predicted outputs are")
+print("DT score was ", score)
+print("Feature importances:", dtree.feature_importances_)
+print("Predicted outputs:")
 print(dtree.predict(X_test))
+print("and the actual outcomes are:")
+print(y_test, "\n")
 
-# and here are the actual labels 
-print("and the actual labels are")
-print(y_test)
+feature_names = df.columns.values
+feature_names2 = []
+for i in range(len(feature_names)):
+    if feature_names[i] != "survived":
+        feature_names2.append(feature_names[i])
 
 
-
-
-
-
-
-#
-# now, show off Random Forests!
-# 
-
-#
-# The data is already in good shape -- a couple of things to define again...
-#
-
-#
-# The first ten will be our test set - the rest will be our training set
-#
-X_test = X_data_full[0:10,0:7]              # the final testing data
-X_train = X_data_full[10:,0:7]              # the training data
-
-y_test = y_data_full[0:10]                  # the final testing outputs/labels (unknown)
-y_train = y_data_full[10:]                  # the training outputs/labels (known)
+target_names = ['0','1'] 
+tree.export_graphviz(dtree, out_file='tree' + str(max_depth) + '.dot',   # constructed filename!
+                            feature_names=feature_names2,  filled=True, rotate=False, # LR vs UD
+                            class_names=target_names, leaves_parallel=True) 
 
 
 #
 # cross-validation to determine the Random Forest's parameters (max_depth and n_estimators)
 #
 #
+max_depth=6
+#
 
-
-# adapt for cross-validation (at least 10 runs w/ average test-score)
+depth = 0
 best_score = 0
-index = 0
-for i in range(1, 11):
-    for j in range(1,11):
-        score = 0
-        rforest = ensemble.RandomForestRegressor(max_depth=i, n_estimators=10)
 
+for n in range(1,max_depth+1):
+
+    rforest = ensemble.RandomForestClassifier(max_depth=n, n_estimators=1000)
+    avg_score = 0
+        
+    for i in range(10):  # run at least 10 times.... take the average testing score
         #
         # split into our cross-validation sets...
         #
@@ -233,103 +170,179 @@ for i in range(1, 11):
         # fit the model using the cross-validation data
         #   typically cross-validation is used to get a sense of how well it works
         #   and tune any parameters, such as the k in kNN (3? 5? 7? 41?, etc.)
-        rforest = rforest.fit(cv_data_train, cv_target_train) 
+        rforest = rforest.fit(cv_data_train, cv_target_train)
         #print("CV training-data score:", dtree.score(cv_data_train,cv_target_train))
         #print("CV testing-data score:", dtree.score(cv_data_test,cv_target_test))
-        score += dtree.score(cv_data_test,cv_target_test)
-    average_score = score/10
-    print(i,average_score)
-    if average_score > best_score:
-        best_score = average_score
-        index = i
-print("randomForest best score", best_score)
-#print("best score is ", best_score)
+        avg_score += rforest.score(cv_data_test,cv_target_test)
+    
+    avg_score = avg_score / 10
+
+    print(n, ":", avg_score)
+
+    if best_score < avg_score:
+        best_score = avg_score
+        depth = n
+
+print("The best RT score was ", best_score, "with a depth of ", depth)
 
 
-# rforest.estimators_  [a list of dtrees!]
 
+X_test = X_data_full[0:42,0:12]              # the final testing data
+X_train = X_data_full[42:,0:12]              # the training data
+y_test = y_data_full[0:42]                  # the final testing outputs/labels (unknown)
+y_train = y_data_full[42:]
 
-#
-# we'll use max_depth == 2
-#
-max_depth = 2
-rforest = ensemble.RandomForestRegressor(max_depth=max_depth, n_estimators=100)
+max_depth = depth
+cv_data_train, cv_data_test, cv_target_train, cv_target_test = \
+            cross_validation.train_test_split(X_train, y_train, test_size=0.2)
+rforest = ensemble.RandomForestClassifier(max_depth=max_depth, n_estimators=1000)
 
-
-#
-# now, train the model with ALL of the training data...  and predict the labels of the test set
-#
-
-X_test = X_data_orig[0:30,0:7]              # the final testing data
-X_train = X_data_orig[30:,0:7]              # the training data
-
-y_test = y_data_orig[0:30]                  # the final testing outputs/labels (unknown)
-y_train = y_data_orig[30:]                  # the training outputs/labels (known)
-
-# this next line is where the full training data is used for the model
 rforest = rforest.fit(X_train, y_train) 
-print("\nCreated and trained a knn Regressor")  #, knn
+score = rforest.score(cv_data_test,cv_target_test)
+print("RT test score:", score)
+print("RT feature importances:", rforest.feature_importances_) 
 
-#
-# feature importances
-#
-print("feature importances:", rforest.feature_importances_)  
+print("The predicted outputs are")
+print(rforest.predict(X_test),"\n")
 
-
-# here are some examples, printed out:
-print("titanic_X_test's predicted outputs are")
-print(rforest.predict(X_test))
-
-# and here are the actual labels 
 print("and the actual labels are")
 print(y_test)
 
 
+############################################ AGES ###########################################################
 
-#
-# Imputing example with titanic data
-#
-WANT_IMPUTING_EXAMPLE = False
-if WANT_IMPUTING_EXAMPLE == True:
-    #
-    # imputing missing values with a random forest...
-    #
-    #
-    # to run this, you'll need _numeric_ labels (0,1,2) instead of 'setosa,'versicolor', etc.
-    #    be sure to update that (way up above!)
+df = pd.read_csv('titanic5.csv', header=0)
+df.head()
+df.info()
 
-    # we _create_ some missing data to show off imputing...
-    #
-    X_data_missing = X_data_full.copy()
-    print("Original:", X_data_missing[-10:,:])
-    y_data_missing = y_data_full.copy()  # we don't mess with this!
+df = df.drop('name', axis=1)
+df = df.drop('ticket', axis=1)
+df = df.drop('fare', axis=1)
+df = df.drop('cabin', axis=1)
+df = df.drop('embarked', axis=1)
+df = df.drop('home.dest', axis=1)
+df = df[np.isfinite(df['pclass'])]
+df = df[np.isfinite(df['survived'])]
 
-    # loop through the last 10 rows and set some to np.nan (not a number)
-    #
-    noise_rate = 0.25
-    NUMROWS, NUMCOLS = X_data_missing.shape
-    for row in range(NUMROWS-10,NUMROWS):
-        for col in range(2,4):
-            if np.random.uniform() < noise_rate:  # noise_rate chance of happening...
-                X_data_missing[row,col] = np.nan
+df['sex'] = df['sex'].map(tr_mf)
 
-    # reassemble data together! np.hstack == horizontal stacking
-    #
-    all_data = np.hstack( [X_data_missing,y_data_missing.reshape(NUMROWS,1)] )
-    print("Missing:", all_data[-10:,:])
+df = df[np.isfinite(df['sex'])]
+df = df[np.isfinite(df['sibsp'])]
+df = df[np.isfinite(df['parch'])]
 
-    from impute import ImputeLearn
-    from sklearn import neighbors
+#replace all NaN ages with -1
+df['age'].fillna(-1, inplace=True)
+df = df.sort('age', ascending=True)
+df['age'] = df['age'].astype(int)
 
-    #
-    # impute with either RFs or KNN :-)
-    #
-    all_data_imp = ImputeLearn( all_data ).impute(learner = ensemble.RandomForestRegressor(n_estimators = 100,max_depth=2))
-    #all_data_imp = ImputeLearn( all_data ).impute(learner = neighbors.KNeighborsRegressor(n_neighbors=5))
-
-    print("Imputed:", all_data_imp[-10:,:])
+count = 0
+for i in range(len(df['age'])):
+    if df['age'][i] == -1:
+        count +=1
 
 
+# extract the underlying data with the values attribute:
+X_data = df.drop('age', axis=1).values        # everything except the 'age' column
+y_data = df[ 'age' ].values
+
+X_data_full = X_data[count:,:]
+y_data_full = y_data[count:]
+
+indicies = np.random.permutation(len(X_data_full))
+X_data_full = X_data_full[indicies]
+y_data_full = y_data_full[indicies]
+
+
+X_test = X_data_full[0:count,0:5]              # the final testing data
+X_train = X_data_full[count:,0:5]              # the training data
+y_test = y_data_full[0:count]                  # the final testing outputs/labels (unknown)
+y_train = y_data_full[count:]                  # the training outputs/labels (known)
+
+
+X_data[:,1] *= 500   #survived
+
+cv_data_train, cv_data_test, cv_target_train, cv_target_test = \
+    cross_validation.train_test_split(X_train, y_train, test_size=0.2)
+rforest = ensemble.RandomForestClassifier(max_depth=depth, n_estimators=1000)
+
+rforest = rforest.fit(X_train, y_train) 
+score = rforest.score(cv_data_test,cv_target_test)
+print("Inputed test score:", score)
+print("Inputed feature importances:", rforest.feature_importances_) 
+
+predicted_values =  rforest.predict(X_test)
+print("The predicted outputs are")
+print(predicted_values,"\n")
+
+print("and the actual labels are")
+print(y_test)
+
+for i in range(len(df['age'])):
+    if df['age'][i] == -1:
+        df['age'][i] = predicted_values[0]
+        predicted_values = predicted_values[1:]
+
+X_data = df.drop('survived', axis=1).values
+y_data = df[ 'survived' ].values
+
+
+X_data_full = X_data[42:,:]
+y_data_full = y_data[42:]
+
+indicies = np.random.permutation(len(X_data_full))
+X_data_full = X_data_full[indicies]
+y_data_full = y_data_full[indicies]
+
+
+X_test = X_data_full[0:42,0:12]             # the final testing data
+X_train = X_data_full[42:,0:12]             # the training data
+y_test = y_data_full[0:42]                  # the final testing outputs/labels (unknown)
+y_train = y_data_full[42:]                  # the training outputs/labels (known)
+
+# feature engineering...
+X_data[:,0] *= 550  #pclass
+X_data[:,1] *= 430  #sex
+X_data[:,2] *= 25  	#age
+X_data[:,3] *= 20   #sibs
+X_data[:,4] *= 1   	#parch
 
 
 
+dtree = tree.DecisionTreeClassifier(max_depth=depth)
+cv_data_train, cv_data_test, cv_target_train, cv_target_test = \
+            cross_validation.train_test_split(X_train, y_train, test_size=0.2) # random_state=0 
+dtree = dtree.fit(cv_data_train, cv_target_train) 
+score = dtree.score(cv_data_test,cv_target_test)
+
+
+
+print("Revised DT score with inputed ages and a depth of ",depth, "was ", score)
+print("Feature importances:", dtree.feature_importances_)
+print("Predicted outputs are")
+print(dtree.predict(X_test))
+print("and the actual outcomes are")
+print(y_test, "\n")
+
+
+"""
+
+DT
+The best DT score was  0.80320855615 with a depth of  5
+DT score was  0.823529411765
+Feature importances: [ 0.17868417  0.6012075   0.14193312  0.07305896  0.00511626]
+Predicted outputs:
+[0 1 0 0 0 0 1 0 0 1 1 0 0 0 0 0 0 0 0 0 1 0 1 0 1 0 0 1 0 0 0 1 1 0 1 1 1
+ 0 0 0 0 0]
+and the actual outcomes are:
+[1 1 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 1 1 0 1 0 1 0 1 0 0 1 0 0 0 0 0 1 1
+ 0 0 0 0 1] 
+
+RT
+The best RT score was  0.806417112299 with a depth of  4
+RT test score: 0.807486631016
+RT feature importances: [ 0.21270386  0.55566693  0.11488716  0.05587033  0.06087171]
+The predicted outputs are
+[0 1 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 1 0 1 0 1 0 0 1 0 0 0 1 1 0 1 1 1
+ 0 0 0 0 0] 
+
+"""
